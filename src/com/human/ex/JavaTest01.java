eaf5713ce11a4729efc0b197a404e9733a804333
asussh <asussh@DESKTOP-PHDIU8N>
2022-03-03 14:26:31 +0900

    test1

---
+++ src/com/human/ex/JavaTest01.java
@@ -0,0 +1,37 @@
+package com.human.ex;
+
+public class JavaTest01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    //ctrl + 방향키 = 단어기준 이동
+		// shift + 방향키 = 영역선택
+		// shift + ctrl = 단어 영역 선택
+		// ctr + /  = 주석 설정 및 해제
+		/* home, end 현줄의 시작점, 끝점으로 이동
+		 * ctr +,- = 글자 폰트 크기조절
+		 * ctr + f 모두 바꾸기
+		 * ctr + z 마지막 실행 취소		
+		 * ctr + y 마지막 실행 취소시킨 것을 재실행
+		 * ctr + c,v,x 복사, 붙여넣기, 잘라넣기
+		*/
+			int a= 3;
+			switch(a) {
+			case 1:
+				System.out.println(1);
+			break;
+			case 2:
+				System.out.println(2);
+			break;
+			case 3:
+				System.out.println(3);
+			break;
+			default:
+				System.out.println(0);
+			} 
+			System.out.print("월");
+			
+			
+	}
+
+}
