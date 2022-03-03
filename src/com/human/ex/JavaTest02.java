f5802d844bd22f024c08409cd5e1bca466a9e8af
asussh <asussh@DESKTOP-PHDIU8N>
2022-03-03 14:31:14 +0900

    test2

--- src/com/human/ex/JavaTest02.java
+++ src/com/human/ex/JavaTest02.java
@@ -14,7 +14,7 @@ public class JavaTest02 {
 		System.out.println("    *\n   **\n  ***\n ****\n*****");
 		System.out.println("*****************\n*\t성적표\t*\n*****************\n* 국어\t 50\t*\n*****************\n* 영어\t 50\t*\n*****************\n* 수학\t 50\t*\n*****************\n* 컴퓨터학과 홍길동 *\n*****************");
 		System.out.println("일\t월\t화\t수\t목\t금\t토\n1\t2\t3\t4\t5\t6\t7\n8\t9\t10\t11\t12\t13\t14\n15\t16\t17\t18\t19\t20\t21\n22\t23\t24\t25\t26\t27\t28\n29\t30\t31");
-		
+		System.out.println("안녕자바");
 	
 	}
 
